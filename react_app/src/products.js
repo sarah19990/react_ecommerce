553b968f42502afd47a89654e83a287a7758231a
sarah19990 <sara1999200407@gmail.com>
2023-05-12 14:53:55 +0100

    added images to the website

---
+++ react_app/src/products.js
@@ -0,0 +1,59 @@
+import product1 from "./assets/products/iphone-14.jpg"
+import product2 from "./assets/products/samsung_galaxy_s23_ultra.jpg"
+import product3 from "./assets/products/huawei_p30_pro.jpg"
+import product4 from "./assets/products/Nokia-X30.jpg"
+import product5 from "./assets/products/google_pixel_7_pro.jpg"
+import product6 from "./assets/products/Doro_8100.jpg"
+import product7 from "./assets/products/sony_xperia_10_IV.jpg"
+import product8 from "./assets/products/motorola_edge_20_pro.jpg"
+
+export const PRODUCTS = [
+    {
+      id: 1,
+      productName: "IPhone 14",
+      price: 764.00,
+      productImage: product1,
+    },
+    {
+        id: 2,
+        productName: "Samsung Galaxy S23 Ultra",
+        price: 800.00,
+        productImage: product2,
+    },
+    {
+        id: 3,
+        productName: "Huawei P30 Pro",
+        price: 400.00,
+        productImage: product3,
+    },
+    {
+        id: 4,
+        productName: "Nokia X30",
+        price: 300.00,
+        productImage: product4,
+    },
+    {
+        id: 5,
+        productName: "Google Pixel 7 Pro",
+        price: 900.00,
+        productImage: product5,
+    },
+    {
+        id: 6,
+        productName: "Doro 8100",
+        price: 200.00,
+        productImage: product6,
+    },
+    {
+        id: 7,
+        productName: "Sony Xperia 10 IV",
+        price: 500.00,
+        productImage: product7,
+    },
+    {
+        id: 8,
+        productName: "Motorola Moto Edge 20 Pro",
+        price: 600.00,
+        productImage: product8,
+    },
+];
