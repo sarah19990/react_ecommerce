6ff17083f7e053d8fee143105e162bf8c76d79a0
sarah19990 <sara1999200407@gmail.com>
2023-05-14 18:14:17 +0100

    changing background color

--- react_app/src/pages/cart/cart.jsx
+++ react_app/src/pages/cart/cart.jsx
@@ -6,21 +6,23 @@ import {CartItem} from './cart-item'
 import './cart.css';
 import { useNavigate } from 'react-router-dom'
 
+
+
 export const Cart = () => {
     const { cartItems, getTotalCartAmount } = useContext(ShopContext);
     const totalAmount = getTotalCartAmount();
 
     const navigate = useNavigate()
   return <div className="cart">
-    <div>
-      <h1> Your Cart Items </h1> 
-    </div>
     <div className="cartItems">
       {PRODUCTS.map((product) => {
           if (cartItems[product.id] !== 0) {
             return <CartItem data={product} />;
         }
     })}
+    </div>
+    <div>
+      <h1> Your Cart Items </h1> 
     </div>
 {totalAmount > 0 ?
     <div className="checkout">
